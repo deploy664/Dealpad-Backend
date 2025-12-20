4f7897afd37f2dd8230a82d8a383bd78c7048707
Ariyan <ariyan@gmail.com>
2025-12-20 18:10:24 +0500

    removed admin panel

--- models/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const adminSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  name: String
-});
-
-module.exports = mongoose.model("Admin", adminSchema);
